ddc22003d6e4ac0f163ccfc3ba0804540ee17f98
n1nja88888 <118420267+n1nja88888@users.noreply.github.com>
2023-08-20 18:19:11 +0800

    Add files via upload

---
+++ fastfoward.js
@@ -0,0 +1,21 @@
+'use strict'
+var speed = -1, timer = null, isRepeated = false
+mp.add_key_binding('right', null, function(event) {
+    var type = event.event
+    if (type == 'down')
+        speed = mp.get_property('speed')
+    else if (type == 'repeat') {
+        mp.set_property('pause', 'no')
+        mp.set_property('speed', 3)
+        mp.osd_message('>>>3.0', 0.1)
+    }
+    else if (type == 'up') {
+        if (mp.get_property('speed') == 3)
+            mp.set_property('speed', speed)
+        else
+            mp.command('seek +5')
+    }
+}, {
+    repeatable: true,
+    complex: true
+})
